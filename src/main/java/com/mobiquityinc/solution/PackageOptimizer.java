7a2c97d1669fc6cdd61750384686621176d5f78a
Tharindu Vishwanath <tharindu.ssl@gmail.com>
2019-02-18 16:31:53 +0100

    added comments to PackageOptimizer

--- src/main/java/com/mobiquityinc/solution/PackageOptimizer.java
+++ src/main/java/com/mobiquityinc/solution/PackageOptimizer.java
@@ -18,23 +18,37 @@ public class PackageOptimizer {
         List<Item> itemsList = parcel.getItemList();
         int numberOfItems = itemsList.size();
 
+        /* create a 2D array matrix with columns equal to maxweight+1 and
+        * rows number of items+1
+        *
+        * For each weight from 0-80, we consider the items that contribute to optimal
+        * package for that particular weight
+        */
         double[][] solutionMatrix = new double[numberOfItems + 1][maxWeight + 1];
 
         int i, j;
 
         for (i = 0; i <= numberOfItems; i++) {
             for (j = 0; j <= maxWeight; j++) {
-                if (i == 0 || i == 0) {
+                if (i == 0 || i == 0) {         // top row and left most column will have 0. First weight that is
+                                                // considered is 0 hence cannot have any item fitting
                     solutionMatrix[i][j] = 0;
                 } else if (Math.ceil(itemsList.get(i - 1).getWeight()) <= j) {
+                    // if the item of the weight is less than the weight that is being considered
+                    // check if adding the item would make it the optimal cost.
+                    // otherwise just use the optimal value without using it
                     solutionMatrix[i][j] = Math.max(solutionMatrix[i - 1][j],
                             solutionMatrix[i - 1][j - (int) Math.floor(itemsList.get(i - 1).getWeight())] + itemsList.get(i - 1).getCost());
 
                 } else
+                    // if the weight of the item is not less than the target weight, that item cannot be considered
+                    // hence the optimal solution without adding the item is considered
                     solutionMatrix[i][j] = solutionMatrix[i - 1][j];
             }
         }
 
+        // by the time the matrix is filled, the cost at the most right bottom index
+        // has the optimal cost of package
         double optimalCostOfPackage = solutionMatrix[numberOfItems][maxWeight];
 
         System.out.println("optimalCostOfPackage-" + optimalCostOfPackage);
