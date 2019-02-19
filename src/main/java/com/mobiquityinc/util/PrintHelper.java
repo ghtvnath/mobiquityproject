52cc368295bbdce3dcc0de766b89652d45964872
Tharindu Vishwanath <tharindu.ssl@gmail.com>
2019-02-19 08:23:30 +0100

    Changed PrinterHelper

--- src/main/java/com/mobiquityinc/util/PrintHelper.java
+++ src/main/java/com/mobiquityinc/util/PrintHelper.java
@@ -2,6 +2,7 @@ package com.mobiquityinc.util;
 
 import com.mobiquityinc.pojo.Item;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -9,21 +10,16 @@ public class PrintHelper {
 
     private static final String DEF_NORES_STR = "-";
 
-    public static String getListOfSelectedItemIdsAsPritableString (List<Item> selectedItems) {
+    public static String getListOfSelectedItemIdsAsPritableString(List<Item> selectedItems) {
         StringBuilder sb = new StringBuilder();
         if (selectedItems == null || selectedItems.size() == 0) {
             sb.append(DEF_NORES_STR);
-        } else{
-            selectedItems.sort(new Comparator<Item>() {
-                @Override
-                public int compare(Item o1, Item o2) {
-                    return o1.getId() - o2.getId();
-                }
-            });
-            for (Item item:selectedItems) {
+        } else {
+            Collections.sort(selectedItems, Comparator.comparing(Item::getId));
+            for (Item item : selectedItems) {
                 sb.append(item.getId()).append(",");
             }
-            sb.deleteCharAt(sb.length()-1);
+            sb.deleteCharAt(sb.length() - 1);
         }
         return sb.toString();
     }
